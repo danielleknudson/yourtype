6af8dcf319ca00675be58fa661a15cc7acbc6b5d
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-06 09:54:43 -0700

    Create basic template for app. Add font list to select drop-down.

--- client/javascripts/views/app.view.js
+++ client/javascripts/views/app.view.js
@@ -1,14 +1,18 @@
 var _ = require('underscore');
 var $ = require('jquery');
 var Backbone = require('backbone');
+var data = require('../fonts.data.js');
 
 var AppView = Backbone.View.extend({
   initialize: function () {
     console.log('AppView created');
   },
+
   template: require('../../templates/app.template.html'),
+
   render: function () {
-    this.$el.html(this.template);
+    console.log(data.fonts);
+    this.$el.html(this.template(data));
     return this.$el;
   }
 });
